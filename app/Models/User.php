dc648e728d24d8b7aefb9913e6278ea237c4c2fa
WizzLike17 <aderay768@gmail.com>
2023-10-26 09:41:05 -0700

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'nim',
+        'password',
+        'remember_token', // Jangan masukkan 'remember_token' ke dalam $fillable
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'password' => 'string', // Menggunakan tipe data 'string' untuk 'password'
+    ];
+}
+
